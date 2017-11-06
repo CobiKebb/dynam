05ebfb3fd623418c718d0d84f1363137a9a10acf
kcarpin <kcarpin@LUBTZLKCARPIN.GAD.SES.COM>
2017-11-06 10:51:14 +0100

    classe B : add getter setter

--- src/dynWebProject/ClasseB.java
+++ src/dynWebProject/ClasseB.java
@@ -3,5 +3,15 @@ package dynWebProject;
 public class ClasseB {
 
 	String idPersonne;
+
+	public String getIdPersonne() {
+		return idPersonne;
+	}
+
+	public void setIdPersonne(String idPersonne) {
+		this.idPersonne = idPersonne;
+	}
+	
+	
 	
 }
